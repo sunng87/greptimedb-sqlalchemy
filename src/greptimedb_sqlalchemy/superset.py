f00865ec3d527fd5229db57a480d4a92f7a5748e
Ning Sun <sunng@protonmail.com>
2024-09-06 14:38:15 -0700

    fix: datatype

--- src/greptimedb_sqlalchemy/superset.py
+++ src/greptimedb_sqlalchemy/superset.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 
+import re
 from typing import Any
-
-from sqlalchemy.engine.reflection import Inspector
 import logging
 
 # Configure the logging
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
-from superset.db_engine_specs.base import BasicParametersType, BaseEngineSpec
-from superset.db_engine_specs.postgres import PostgresEngineSpec
-
+from sqlalchemy.engine.reflection import Inspector
 from superset.constants import TimeGrain
+from superset.db_engine_specs.base import BasicParametersType, BaseEngineSpec
+from sqlalchemy.types import TypeEngine
+from superset.utils.core import GenericDataType, ColumnSpec, ColumnTypeSource
+
+from . import types
 
 
 class GreptimeDBEngineSpec(BaseEngineSpec):
@@ -45,6 +47,99 @@ class GreptimeDBEngineSpec(BaseEngineSpec):
         TimeGrain.YEAR: "DATE_TRUNC('year', {col})",
     }
 
+    column_type_mappings = (
+        (
+            re.compile("^BOOLEAN$", re.IGNORECASE),
+            types.Boolean,
+            GenericDataType.BOOLEAN,
+        ),
+        (
+            re.compile("^BINARY$", re.IGNORECASE),
+            types.Binary,
+            GenericDataType.STRING,
+        ),
+        (
+            re.compile("^STRING$", re.IGNORECASE),
+            types.String,
+            GenericDataType.STRING,
+        ),
+        (
+            re.compile("^INT8$", re.IGNORECASE),
+            types.Int8,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^INT16$", re.IGNORECASE),
+            types.Int16,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^INT32$", re.IGNORECASE),
+            types.Int32,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^INT64$", re.IGNORECASE),
+            types.Int64,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^UINT8$", re.IGNORECASE),
+            types.UInt8,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^UINT16$", re.IGNORECASE),
+            types.UInt16,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^UINT32$", re.IGNORECASE),
+            types.UInt32,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^UINT64$", re.IGNORECASE),
+            types.UInt64,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^DECIMAL", re.IGNORECASE),
+            types.Decimal,
+            GenericDataType.NUMERIC,
+        ),
+        (
+            re.compile("^DATE$", re.IGNORECASE),
+            types.Date,
+            GenericDataType.TEMPORAL,
+        ),
+        (
+            re.compile("^DATETIME$", re.IGNORECASE),
+            types.DateTime,
+            GenericDataType.TEMPORAL,
+        ),
+        (
+            re.compile("^TIMESTAMPSECOND$", re.IGNORECASE),
+            types.TimestampSecond,
+            GenericDataType.TEMPORAL,
+        ),
+        (
+            re.compile("^TIMESTAMPMILLISECOND$", re.IGNORECASE),
+            types.TimestampMillisecond,
+            GenericDataType.TEMPORAL,
+        ),
+        (
+            re.compile("^TIMESTAMPMICROSECOND$", re.IGNORECASE),
+            types.TimestampMicrosecond,
+            GenericDataType.TEMPORAL,
+        ),
+        (
+            re.compile("^TIMESTAMPNANOSECOND$", re.IGNORECASE),
+            types.TimestampNanosecond,
+            GenericDataType.TEMPORAL,
+        ),
+    )
+
     @classmethod
     def build_sqlalchemy_uri(
         cls,
@@ -76,11 +171,6 @@ class GreptimeDBEngineSpec(BaseEngineSpec):
         return set()
 
     @classmethod
-    def get_table_comment(cls, inspector, table) -> str | None:
-        logger.info(f"getting table comment {table}")
-        return None
-
-    @classmethod
     def get_indexes(
         cls,
         database,
@@ -92,3 +182,47 @@ class GreptimeDBEngineSpec(BaseEngineSpec):
     @classmethod
     def get_table_names(cls, database, inspector, schema: str | None) -> set[str]:
         return set(inspector.get_table_names(schema))
+
+    @classmethod
+    def get_column_types(
+        cls,
+        column_type: str | None,
+    ) -> tuple[TypeEngine, GenericDataType] | None:
+        if not column_type:
+            return None
+
+        for regex, sqla_type, generic_type in (
+            cls.column_type_mappings + cls._default_column_type_mappings
+        ):
+            match = regex.match(column_type)
+
+            if not match:
+                continue
+
+            logger.info(f"getting column type {column_type}, {sqla_type}")
+            if callable(sqla_type):
+                return sqla_type(match), generic_type
+            return sqla_type, generic_type
+        return None
+
+    @classmethod
+    def get_column_spec(
+        cls,
+        native_type: str | None,
+        db_extra: dict[str, Any] | None = None,
+        source: ColumnTypeSource = ColumnTypeSource.GET_TABLE,
+    ) -> ColumnSpec | None:
+        spec = GreptimeDBEngineSpec.get_column_types(native_type)
+        if spec is not None:
+            sqla_type, generic_type = spec
+            return ColumnSpec(
+                sqla_type,
+                generic_type,
+                generic_type == GenericDataType.TEMPORAL,
+            )
+        else:
+            return None
+
+    @classmethod
+    def column_datatype_to_string(cls, sqla_column_type: TypeEngine, dialect) -> str:
+        return sqla_column_type.__visit_name__
